1c106546561a6d752ebcc72d5f995eb9cbae1c63
EngyMohamed5 <engymohamedalratmy@gmail.com>
2024-11-22 21:28:35 +0200

    histogramEqualization function

--- contrastStretching.m
+++ contrastStretching.m
@@ -11,11 +11,12 @@ function stretchedImage = contrastStretching(Image, newMin, newMax)
     stretchedImage = uint8(stretchedImage);
     
   
-    subplot(1, 2, 1);
-    imshow(uint8(Image));
-    title('Original Grayscale Image');
+  
+    figure;
+    subplot(2, 2, 1); imshow(uint8(Image)); title('Original Image');
+    subplot(2, 2, 2); imhist(uint8(Image)); title('Original Histogram');
+    subplot(2, 2, 3); imshow(stretchedImage); title('Contrast Stretched Image');
+    subplot(2, 2, 4); imhist(stretchedImage); title('Contrast Stretched Histogram');
     
-    subplot(1, 2, 2);
-    imshow(stretchedImage);
-    title(['Contrast Stretched Image (', num2str(newMin), '-', num2str(newMax), ')']);
+    
 end
--- histogramEqualization.m
+++ histogramEqualization.m
@@ -1,19 +1,29 @@
-function equalizedImage = histogramEqualization(grayImage)
-    equalizedImage = histeq(grayImage);
-
-    subplot(2, 2, 1);
-    imshow(uint8(grayImage));
-    title('Original Grayscale Image');
-
-    subplot(2, 2, 2);
-    imhist(uint8(grayImage));
-    title('Histogram of Original Image');
-
-    subplot(2, 2, 3);
-    imshow(equalizedImage);
-    title('Equalized Image');
-
-    subplot(2, 2, 4);
-    imhist(equalizedImage);
-    title('Histogram of Equalized Image');
+function outputImage = histogramEqualization(inputImage)
+    
+    if size(inputImage, 3) == 3
+        inputImage = rgb2gray(inputImage);
+    end
+    
+    [counts, binLocations] = imhist(inputImage);
+    
+  
+    numPixels = numel(inputImage);
+    normalizedCounts = counts / numPixels;
+    
+    % Compute the cumulative distribution function (CDF)
+    cdf = cumsum(normalizedCounts);
+    
+    % Scale the CDF to fit the intensity range [0, 255]
+    cdfScaled = round(cdf * 255);
+    
+    % Map the intensity values based on the CDF
+    outputImage = cdfScaled(inputImage + 1);
+     
+    outputImage = uint8(outputImage);
+   
+    figure;
+    subplot(2, 2, 1); imshow(inputImage); title('Original Image');
+    subplot(2, 2, 2); imhist(inputImage); title('Original Histogram');
+    subplot(2, 2, 3); imshow(outputImage); title('Equalized Image');
+    subplot(2, 2, 4); imhist(outputImage); title('Equalized Histogram');
 end
